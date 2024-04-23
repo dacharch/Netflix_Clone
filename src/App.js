170304e1d80a81dbd6f36a54aa0985a2e85c1cc8
Neeraj Kumar <nk35715945@gmail.com>
2024-04-23 09:52:04 +0530

    faq is added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Feature from './components/Features/Feature';
 import Home from './components/Home/Home';
+import { Question } from './components/Questions/Question';
 
 function App() {
   return (
     <>
       <Home />
       <Feature />
+      <Question/>
     </>
   );
 }
--- src/components/Features/Feature.js
+++ src/components/Features/Feature.js
@@ -4,52 +4,54 @@ import "./Feature.css"
 
 const Feature = () => {
   return (
-    <div className="features">
-      <div className="row">
-        <div className="text-col">
-          <h2>Enjoy on your TV</h2>
-          <p>
-            Watch on smart TVs, PlayStation,Xbox,Chromecast Apple TV,Blu-ray
-          </p>
+    <>
+      <div className="feature-container">
+        <div className="features">
+          <div className="row">
+            <div className="text-col">
+              <h2>Enjoy on your TV</h2>
+              <p>
+                Watch on smart TVs, PlayStation,Xbox,Chromecast Apple TV,Blu-ray
+              </p>
+            </div>
+            <div className="img-col">
+              <img src={assets.feature} />
+            </div>
+          </div>
         </div>
-        <div className="img-col">
-          <img src={assets.feature} />
-        </div>
-      </div>
+        <div className="border-line "></div>
 
-      <div className="row">
-        <div className="img-col">
-          <img src={assets.feature2} />
+        <div className="features">
+          <div className="row">
+            <div className="img-col">
+              <img src={assets.feature2} />
+            </div>
+            <div className="text-col">
+              <h2>Download your shows to watch offline</h2>
+              <p>
+                Save your favourites easily and always have something to watch.
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="text-col">
-          <h2>Download your shows to watch offline</h2>
-          <p>Save your favourites easily and always have something to watch.</p>
-        </div>
-      </div>
+        <div className="border-line "></div>
 
-      <div className="row">
-        <div className="text-col">
-          <h2>Watch everywhere</h2>
-          <p>
-            Stream unlimited movies and TV shows on your phone, tablet, laptop,
-            and TV.
-          </p>
+        <div className="features">
+          <div className="row">
+            <div className="img-col">
+              <img src={assets.feature4} />
+            </div>
+            <div className="text-col">
+              <h2>Download your shows to watch offline</h2>
+              <p>
+                Save your favourites easily and always have something to watch.
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="img-col">
-          <img src={assets.feature3} />
-        </div>
+        <div className="border-line "></div>
       </div>
-
-      <div className="row">
-        <div className="img-col">
-          <img src={assets.feature4} />
-        </div>
-        <div className="text-col">
-          <h2>Download your shows to watch offline</h2>
-          <p>Save your favourites easily and always have something to watch.</p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
